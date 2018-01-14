1a71db9cdb8b60da25cd60e73fb08463eea1bbb6
Victor Perron <victor@iso3103.net>
2018-01-14 20:33:09 +0100

    admin: Stop needing the time to be set

--- backent/api/admin.py
+++ backent/api/admin.py
@@ -1,8 +1,27 @@
+from time import gmtime, strftime
+
 from django.contrib import admin
+from django.db import models as django_models
 
 from . import models
+
+
+class MyDate(admin.widgets.AdminSplitDateTime):
+
+    def __init__(self, attrs=None):
+        super().__init__(attrs)
+
+    def value_from_datadict(self, data, files, name):
+        value = super().value_from_datadict(data, files, name)
+        if not value[1]:
+            value[1] = strftime('%H:%M:%S', gmtime())
+        return value
 
 
 @admin.register(models.Event, models.Location, models.Organization)
 class EventAdmin(admin.ModelAdmin):
     exclude = ('slug',)
+
+    formfield_overrides = {
+        django_models.DateTimeField: {'widget': MyDate},
+    }
